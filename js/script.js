674c0be3700fccd241da5393f952ff2cb1121ca6
Vicki Crescencio <victoriaacw92@gmail.com>
2020-06-23 23:59:33 -0700

    Updated JS file

--- js/script.js
+++ js/script.js
@@ -17,8 +17,13 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+// create a variable to store student list item elements in the student-list
+const studentLists = document.querySelectorAll('.student-list li');
 
+const itemsPerPage = 10;
 
+//reference the main page elements inside of the parent div
+const page = document.getElementsByClassName('.page');
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,13 +40,64 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+const showPage = (list, page) => {
+    
+    const startIndex = (page * itemsPerPage) - itemsPerPage;
+    const endIndex = (page * itemsPerPage);
+    
+/*
+Loop over items in the list parameter
+-- If the index of a list item is >= the index of the first item that should be shown on the page
+-- && the list item index is <= the index of the last item that should be shown on the page, show it
+*/
+    
+     for (let i = 0; i < list.length; i += 1) {
+         if (i >= startIndex && i <= endIndex) {
+             list[i].style.display = '';
+             
+         } else {
+             list[i].style.display = "none";
+         }
 
-
+    }
+   
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+
+const appendPageLinks = (list) => {
+ /*
+5. Add an event listener to each a tag. When they are clicked
+call the showPage function to display the appropriate page
+6. Loop over pagination links to remove active class from all links
+7. Add the active class to the link that was just clicked. You can identify that
+clicked link using event.target 
+*/
+    // 1. Determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
+    
+    const numberOfPages = Math.ceil(list.length / itemsPerPage);
+
+    // 2. Create a div, give it the “pagination” class, and append it to the .page div
+    const pagDiv = document.createElement('div');
+    div.className = 'pagination';
+    page.appendChild(pagDiv);
+
+    // 3. Add a ul to the “pagination” div to store the pagination links
+    const ul = document.createElement('ul');
+    pagDiv.appendChild(ul);
+
+    // 4. for every page, add li and a tags with the page number text
+    let li = document.createElement('li');
+    ul.appendChild(li);
+
+    const a = document.createElement('a');
+    a.href = "#";
+    a.textContent = i;
+    li.appendChild(a);
+}
 
 
 
