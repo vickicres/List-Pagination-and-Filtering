cb0ffd55f9dd8270d8a6c0e25a99b1293a0f2b0c
Vicki Crescencio <victoriaacw92@gmail.com>
2020-06-29 00:18:14 -0700

    Final update JS file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 #Techdegree Porject 2
  This is my project 2
+ 
+ I am going for Exceeds expectation, if not meet the expectation, please rejected my project and let me know what I am missing and what needs to improve.
+ 
+ 
--- js/script.js
+++ js/script.js
@@ -3,149 +3,174 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 // create a variable to store student list item elements in the student-list
 const studentListItems = document.querySelectorAll('.student-item');
-//console.log(studentListItems);
 
-//create variable to store the number of items per each page
-const itemsPerPage = 10;
+//create variable to store the number of students per each page
+const studentsPerPage = 10;
 
-//reference the main page elements inside of the parent div
-const page = document.querySelector('.page');
-
-
-/*** 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 // Create the `showPage` function to hide all of the items in the list except for the ten you want to show.
-function showPage(list, page) {
-
+const showPage = (list,page) => {
+ 
     //Create two variables to store the start index and the end index of the list items to be displayed on the given page
-    const startIndex = (page * itemsPerPage) - itemsPerPage;
-    const endIndex = (page * itemsPerPage) - 1;
-
-    //loop over the list and hide all the list items except for the 10 items to show at the start page
-    for (let i = 0; i < list.length; i += 1) {
-        list[i].style.display = 'none';
-    }
+    const startIndex = (page * studentsPerPage) - studentsPerPage;
+    const endIndex = page * studentsPerPage;
+    
     /*
     Loop over items in the list parameter
     -- If the index of a list item is >= the index of the first item that should be shown on the page
     -- && the list item index is <= the index of the last item that should be shown on the page, show it
     */
-    for (let i = 0; i < list.length; i += 1) {
-        if (i >= startIndex && i <= endIndex) {
-            list[i].style.display = '';
-        }
-    }
-}
-
-//showPage(studentListItems, 1);
+  for (let i = 0 ; i < list.length; i += 1) { 
+      if(i >= startIndex && i < endIndex){
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
+      }
+   }
+   
+};
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-function appendPageLinks(list) {
+const appendPageLinks = (list) => {
+    
+    //reference the main page elements inside of the parent div
+    const mainDiv = document.querySelector('.page');
+    
+    //To prevent the pagination link form building up everytime when input some search values.
+    const pagLinks = document.querySelector('.pagination');
+    if (pagLinks !== null) {
+        mainDiv.removeChild(pagLinks);
+    } 
+     // 1. Determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
 
-    // 1. Determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
-
-    const numberOfPages = Math.max(list.length / itemsPerPage);
+    const numbersOfPages = Math.ceil(list.length / studentsPerPage);
 
     // 2. Create a div, give it the “pagination” class, and append it to the .page div
-    const pagDiv = document.createElement('div');
-    pagDiv.className = 'pagination';
-    page.appendChild(pagDiv);
-
     // 3. Add a ul to the “pagination” div to store the pagination links
-    const ul = document.createElement('ul');
-    pagDiv.appendChild(ul);
+   const pagDiv = document.createElement('div');
+   const pagUl = document.createElement('ul');
+   pagDiv.className = 'pagination';
+   mainDiv.appendChild(pagDiv);
+   pagDiv.appendChild(pagUl);
+    
+    
+    // If the student names does not include a value in the user input, a text will be displayed in the HTML that says 'No Results Found'.
+   if (list.length === 0) { 
+      const noResults = document.createElement('span');
+      noResults.textContent = 'No Results Found';
+      noResults.style.color = 'red';
+      noResults.style.textTransform = 'uppercase';
+      noResults.style.fontSize = '20px';
+      pagDiv.appendChild(noResults);
+   }
 
     // 4. for every page, add li and a tags with the page number text
-    for (let i = 0; i < numberOfPages; i += 1) {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        ul.appendChild(li);
-        li.appendChild(a);
+    for (let i = 0; i < numbersOfPages; i += 1) {
+        const pagLi = document.createElement('li');
+        const pagATag = document.createElement('a');
         // add # to a tag
-        a.href = '#';
+         pagATag.href = '#';
+        pagATag.textContent = i + 1;
         //use if statement to set the start page to 1 instead 0
-        a.textContent = i + 1;
-        if (i == 0) {
-            a.className = 'active';
+        if (i === 0) {
+             pagATag.className = 'active';
         }
-
+        pagUl.appendChild(pagLi);
+        pagLi.appendChild(pagATag);  
     }
 
     //5. Add an event listener to each a tag. When they are clicked call the showPage function to display the appropriate page
-    // select the ul child element
-        const links = document.querySelector('div.pagination').firstElementChild;
     
-        for (let i = 0; i < links.children.length; i += 1) {
-    
-            links.children[i].addEventListener('click', (e) => {
-                e.preventDefault();
-                showPage(list, links.children[i].firstElementChild.textContent);
-    //6. Loop over pagination links to remove active class from all appendPageLinks   
-                for (let i = 0; i < links.children.length; i += 1) { 
-                    links.children[i].firstElementChild.classList.remove('active');
-                }
-     //7. Add the active class to the link that was just clicked. You can identify that clicked link using event.target           
-                e.target.className = 'active';
-    
-            });  
-        }
-}
+   const a = document.querySelectorAll('a');
+   //This loop is used in adding event listener to each 'a' element.
+   for (let i = 0; i < a.length; i += 1){
+      a[i].addEventListener('click', (e) => {
+         for(let i = 0; i < a.length; i += 1){ // This loop removes 'active' class name in all page links.
+            a[i].classList.remove('active');
+         };
+         e.target.classList.add('active'); // Adds the 'active' class name only to the clicked page link.
+         showPage(list, e.target.textContent); //Calling this function will show the student list of the clicked page.
+      });
+   }
+
+};
+
+
 
 // Extra creadit: create a search bar
-const searchDiv = document.querySelector('.page-header');
+
 // create a div and give it class name "student-search" and append it to the .page div
-const searchBar = document.createElement('div');
-searchBar.className = 'student-search';
-searchDiv.appendChild(searchBar);
+const addSearch = () => {
+    const pageDiv = document.querySelector('.page-header');
+    
+    // create search div
+    const searchDiv = document.createElement('div');
+    searchDiv.className = 'student-search';
+    pageDiv.appendChild(searchDiv);
 
-//create an element to strore valueable
-const input = document.createElement('input');
-input.placeholder = 'Search for students...';
-searchBar.appendChild(input);
+    //create a search input element to strore valueable
+    const searchInput = document.createElement('input');
+    searchInput.placeholder = 'Search for students...';
+    searchDiv.appendChild(searchInput);
 
-// create a button
-const button = document.createElement('button');
-button.textContent = 'Search';
-searchBar.appendChild(button);
+    // create a search button
+    const searchButton = document.createElement('button');
+    searchButton.textContent = 'Search';
+    searchDiv.appendChild(searchButton);
+    
+    //add event listener to the search button
+    searchButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      const inputValue = searchInput.value;
+      const results = searchFilter(inputValue, studentListItems);
+      showPage(results, 1);
+      appendPageLinks(results);
+   });
+    
+   // add event listener to the  search input
+   searchInput.addEventListener('keyup', (e) => {
+      const results =  searchFilter(e.target.value, studentListItems);
+      showPage(results, 1);
+      appendPageLinks(results);
+
+   });
+    
+};
 
 
+// Display the search result
+const searchFilter = (input, names) => {
+//    const inputValue = searchInput.value;
+    const searchResults = []; // set search result as an empty array to store values.
+    // if the input field is emptied, it will return to the original student list.
+    if (input === null) {
+        return list; 
+    } else {
+        // otherwise this will loops through all the student list to find the matching result
+        for (let i = 0; i < names.length; i += 1) {
+        //call out the h3 element that conatins student names in each list
+        const studentNames = names[i].querySelector('h3').textContent.toLowerCase();
+        names[i].style.display = 'none';
+        // if the student names match the input result, then the list will push out the value inside of the arrays
+        if (studentNames.includes(input.toLowerCase())) {
+            searchResults.push(names[i]);
+        } 
+    }      
+        return searchResults;
+    }
+};
 
+
+addSearch();
 // show the paginations on the bottom of the list
 showPage(studentListItems, 1);
 // it will show the list of students on the page when the page button was clicked
 appendPageLinks(studentListItems);
 
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
